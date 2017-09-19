cb0f53fe02e262aeb347c0d8f6dd9542b4c433e2
Hung Nguyen Dang <hungnguyendang@hungs-mbp.ie.deri.local>
2017-09-19 15:18:58 +0100

    init

---
+++ src/main/java/io/jlike/message/bootermessage/BooterMessageApplication.java
@@ -0,0 +1,12 @@
+package io.jlike.message.bootermessage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooterMessageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooterMessageApplication.class, args);
+	}
+}
---
+++ src/test/java/io/jlike/message/bootermessage/BooterMessageApplicationTests.java
@@ -0,0 +1,16 @@
+package io.jlike.message.bootermessage;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BooterMessageApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
